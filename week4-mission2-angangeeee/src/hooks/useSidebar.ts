308a45907fc18b2031d631fd27baeca5ddc06ea7
yangyangeeee <sophia_ysy@naver.com>
2026-05-23 06:16:11 +0900

    ✨ Feat: 미션3 Sidebar 다시 생각해보기

---
+++ week4-mission2-angangeeee/src/hooks/useSidebar.ts
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useSidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  const toggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return {
+    isOpen,
+    open,
+    close,
+    toggle,
+  };
+}
